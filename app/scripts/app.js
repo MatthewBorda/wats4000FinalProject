29928323a6e8c3df6e176e65efc0ca6245dd7f08
MatthewBorda <MatthewMBorda@Gmail.com>
2016-07-20 20:54:42 -0700

    Grunt build control
    Initial deployment of Angular app

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name wats4000FinalProjectApp
+ * @description
+ * # wats4000FinalProjectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('wats4000FinalProjectApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
