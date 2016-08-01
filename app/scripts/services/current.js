f0cf6278ed27a9a7c8d828a770da1de4f417a0ac
matthewmborda <matthewmborda@gmail.com>
2016-07-31 22:33:35 -0400

    workspaceApp

--- app/scripts/services/current.js
+++ app/scripts/services/current.js
@@ -1,44 +1,25 @@
-// 'use strict';
-
-// /**
-//  * @ngdoc service
-//  * @name workspaceApp.current
-//  * @description
-//  * # current
-//  * Factory in the workspaceApp.
-//  */
-// angular.module('workspaceApp')
-//   .factory('current', function ($resource) {
-//     // Service logic
-//     // ...
-
-//     // Public API here
-//     return $resource('http://api.openweathermap.org/data/2.5/weather?q=:location&units=imperial&APPID=72c36323cb40ea6bbfa8cd2b63040617', {}, {
-//       query: {
-//         method:'GET',
-//         params:{
-//           location: 'Seattle,us'
-//         },
-//         isArray:false
-//       }
-//     });
-//   });
 'use strict';
 
 /**
- * @ngdoc function
- * @name yourApp.controller:MainCtrl
+ * @ngdoc service
+ * @name workspaceApp.current
  * @description
- * # MainCtrl
- * Controller of the yourApp
+ * # current
+ * Factory in the workspaceApp.
  */
 angular.module('workspaceApp')
-  .controller('MainCtrl', function ($scope, current) {
-    $scope.current = current.query();
+  .factory('current', function ($resource) {
+    // Service logic
+    // ...
 
-    $scope.refreshCurrent = function(){
-        $scope.current = current.query({
-            location: $scope.location
-        });
-    };
+    // Public API here
+    return $resource('http://api.openweathermap.org/data/2.5/weather?q=:location&units=imperial&APPID=72c36323cb40ea6bbfa8cd2b63040617', {}, {
+      query: {
+        method:'GET',
+        params:{
+          location: 'Seattle,us'
+        },
+        isArray:false
+      }
+    });
   });
