3f06e83a2bbd220c7181c54394937a307b3d42cb
matthewmborda <matthewmborda@gmail.com>
2016-07-31 22:24:16 -0400

    workspaceApp

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,22 @@
 
 // myApp? wats4000FinalProject workspaceApp
 angular.module('workspaceApp')
-  .controller('MainCtrl', function ($scope, searchbox) {
-    $scope.searchbox = searchbox.query();
-  
-    $scope.refreshSearchBox = function(){
-        $scope.searchbox = searchbox.query({
+  .controller('MainCtrl', function ($scope, current) {
+    $scope.current = current.query();
+
+    $scope.refreshCurrent = function(){
+        $scope.current = current.query({
             location: $scope.location
         });
     };
   });
+//   .controller('MainCtrl', function ($scope, searchbox) {
+//     $scope.searchbox = searchbox.query();
+  
+//     $scope.refreshSearchBox = function(){
+//         $scope.searchbox = searchbox.query({
+//             location: $scope.location
+//         });
+//     };
+//   });
 
