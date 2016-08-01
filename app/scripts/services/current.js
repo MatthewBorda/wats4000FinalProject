54541ce7dd39d975105ab9706d66c8d8ee016803
matthewmborda <matthewmborda@gmail.com>
2016-07-31 22:41:17 -0400

    workspaceApp

--- app/scripts/services/current.js
+++ app/scripts/services/current.js
@@ -13,7 +13,7 @@ angular.module('workspaceApp')
     // ...
 
     // Public API here
-    return $resource('http://api.openweathermap.org/data/2.5/weather?q=:location&units=imperial&APPID=72c36323cb40ea6bbfa8cd2b63040617', {}, {
+    return $resource('http://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial&APPID=72c36323cb40ea6bbfa8cd2b63040617', {}, {
       query: {
         method:'GET',
         params:{
